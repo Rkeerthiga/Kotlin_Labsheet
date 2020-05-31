2b67d3b7f782a4b6e400b52f89e84601240bb8a0
Rkeerthiga <keerthigaputtur21@gmail.com>
2020-05-31 13:11:29 +0530

    Kotlin_Labsheet

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.loginform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_click = findViewById<Button>(R.id.btnLogin)
+        button_click.setOnClickListener{
+            val intent = Intent(this,Login::class.java)
+            startActivity(intent)
+        }
     }
 }
